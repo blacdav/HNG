b59b68a63a1577c4ed76b7b3ecd5a19e5986a68a
blacdav <davidaniefoik@gmail.com>
2026-04-16 15:04:50 +0100

    update the db config

--- stage0/backend/src/db/config.js
+++ stage0/backend/src/db/config.js
@@ -1,13 +1,30 @@
+// import { sequelize } from "./index.js";
+
+// export const dbConn = async () => {
+//   try {
+//     await sequelize.authenticate();
+//     // await sequelize.sync({ alter: true });
+//     console.log("Database connected successfully");
+//   } catch (err) {
+//     console.error("Database connection failed:", err);
+//     throw err;
+//     // process.exit(1);
+//   }
+// };
+
 import { sequelize } from "./index.js";
 
+let isConnected = false;
+
 export const dbConn = async () => {
+  if (isConnected) return;
+
   try {
     await sequelize.authenticate();
-    // await sequelize.sync({ alter: true });
+    isConnected = true;
     console.log("Database connected successfully");
   } catch (err) {
     console.error("Database connection failed:", err);
     throw err;
-    // process.exit(1);
   }
 };
