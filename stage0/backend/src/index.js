90744ec54de787a4fb2484e3b73fc75ecab39cd9
blacdav <davidaniefoik@gmail.com>
2026-04-16 13:55:21 +0100

    rewrote the classify endpoint in its controller

--- stage0/backend/src/index.js
+++ stage0/backend/src/index.js
@@ -12,7 +12,7 @@ app.use(cors({
     credentials: true
 }));
 
-app.use(express.static("public"));
+// app.use(express.static("public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
