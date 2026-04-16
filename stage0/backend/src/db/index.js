8e9c648645768227336fcaf5e165423a3d6c6b1d
blacdav <davidaniefoik@gmail.com>
2026-04-16 20:37:40 +0100

    new update

--- stage0/backend/src/db/index.js
+++ stage0/backend/src/db/index.js
@@ -1,14 +1,18 @@
 import { Sequelize } from "sequelize";
 import { dbConfig } from "../config/index.js";
+import fs from "fs";
+
+const sslCa = Buffer.from(dbConfig.ssl, "base64").toString("utf-8");
 
 export const sequelize = new Sequelize(dbConfig.name, dbConfig.user, dbConfig.password, {
     host: dbConfig.host,
     port: dbConfig.port,
     dialect: dbConfig.dialect,
     ssl: {
-        ca: dbConfig.ssl
+        ca: sslCa
     },
-    logging: true,
+    logging: console.log,
+    // logging: (...msg) => console.log(msg),
     pool: {
         max: 10,
         min: 1,
