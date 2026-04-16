5fdd187bdac2e57e9842a502096d2c0687382d4f
blacdav <davidaniefoik@gmail.com>
2026-04-16 15:47:41 +0100

    update11

--- stage0/backend/src/db/index.js
+++ stage0/backend/src/db/index.js
@@ -3,11 +3,12 @@ import { dbConfig } from "../config/index.js";
 
 export const sequelize = new Sequelize(dbConfig.name, dbConfig.user, dbConfig.password, {
     host: dbConfig.host,
-    port: dbConfig.port,
+    port: Number(dbConfig.port),
     dialect: dbConfig.dialect,
-    dialectOptions: dbConfig.ssl,
-    // ssl: {
-    //     ca: dbConfig.ssl
-    // },
+    dialectOptions: dbConfig.ssl ? {
+        ssl: {
+            ca: dbConfig.ssl
+        },
+    } : {},
     logging: true
 });
