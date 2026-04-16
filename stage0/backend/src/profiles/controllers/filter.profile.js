727e96921a254eec70b54d7e791129b4cc326aea
blacdav <davidaniefoik@gmail.com>
2026-04-16 19:12:10 +0100

    added the filter logic for the /profile endpoint

---
+++ stage0/backend/src/profiles/controllers/filter.profile.js
@@ -0,0 +1,51 @@
+import models from "../../models/index.model.js";
+
+const { Profile } = models;
+
+export const FilterProfile = async (req, res) => {
+    const { age_group, gender, country_id } = req.query;
+
+    let filter = {};
+
+    if (!age_group && !gender && !country_id) {
+        return res.status(400).json({
+            status: "error",
+            message: "query param is required"
+        })
+    }
+
+    if (typeof age_group === "string" && age_group.trim() !== "") {
+        filter.age_group = age_group;
+    }
+
+    if (typeof gender === "string" && gender.trim() !== "") {
+        filter.gender = gender;
+    }
+
+    if (typeof country_id === "string" && country_id.trim() !== "") {
+        filter.country_id = country_id;
+    }
+
+    try {
+        const {count, rows:profile} = await Profile.findAndCountAll({ where: filter });
+
+        if (!profile) {
+            return res.status(404).json({
+                status: "success",
+                message: "Profile not found!"
+            })
+        }
+
+        return res.status(200).json({
+            status: "success",
+            count,
+            data: profile
+        })
+    } catch (err) {
+        return res.status(500).json({
+            status: "error",
+            message: "Something went wrong",
+            error: err.message
+        });
+    }
+}
